de849d25f35b2fde28f932962d638ff7796ec1c6
Chris <chris.nykon@gmail.com>
2017-06-02 20:14:55 +1200

    AutoInstall v0.3
    Updated script. Installs WannaCry branch of Dionaea. Addressed broken Cowrie install. Tested on fresh Ubuntu 14.04 t2.micro instance.

--- honeypot/autoinstall.sh
+++ honeypot/autoinstall.sh
@@ -1,136 +1,180 @@
 #!/bin/bash
 
-# Honeypot Autoinstall Script v0.2
+# Honeypot AutoInstall Script v0.3
 # by Chris Campbell
 #
 # Twitter: @phage_nz
 # GitHub: phage-nz
-# Blog: bytefog.blogspot.com
+# Blog: https://phage.nz
 
 # Installs:
 # Dionaea
 # p0f
 # Cowrie
 #
-# Tested on Ubuntu 16.04 (EC2 t2.micro instance)
+# Tested on Ubuntu 14.04 (EC2 t2.micro instance)
 
 # Variables:
+INSTALL_DIONAEA="yes" # yes or no.
+INSTALL_COWRIE="no" # yes or no.
+
 SSL_C="US" # Country code.
-SSL_CN="server.domain.com" # Certificate CN.
-SSL_O="Company Name Inc."  # Company name.
+SSL_CN="example.company.com" # Certificate CN.
+SSL_O="Example Company Ltd."  # Company name.
 SSL_OU="OU" # Organisational unit.
-DOMAIN="DOMAIN-GROUP" # Domain name.
-SERVER="SERVER-DOMAIN" # Server name.
+
+DOMAIN="WORKGROUP" # Domain name.
+SERVER="EXAMPLE-SERVER" # Server name.
+
+VT_KEY="" # Your VirusTotal API key.
 
 #
 # SCRIPT START
 #
 
-echo "Updating server..."
-sudo apt-get update
-sudo apt-get upgrade -y
-echo "Installing dependencies..."
-sudo apt-get install software-properties-common python-software-properties p0f autoconf automake check cython3 libcurl4-openssl-dev libemu-dev libev-dev libglib2.0-dev libloudmouth1-dev libnetfilter-queue-dev libnl-dev libpcap-dev libreadline-dev libsqlite3-dev libssl-dev libtool libudns-dev libxml2-dev libxslt1-dev python3 python3-dev python3-yaml -y
-echo "Installing Dionaea..."
-cd /opt
-git clone https://github.com/DinoTools/dionaea.git
-cd dionaea
-git clone https://github.com/DinoTools/dionaea
-cd dionaea
-git clone https://github.com/gento/liblcfg
-cd liblcfg/code
-autoreconf -vi
-./configure --prefix=/opt/dionaea
-make install
-cd ..
-cd ..
-autoreconf -vi
-./configure \
-	--disable-werror \
-	--prefix=/opt/dionaea \
-	--with-python=/usr/bin/python3 \
-	--with-cython-dir=/usr/bin \
-	--with-ev-include=/usr/include \
-	--with-ev-lib=/usr/lib \
-	--with-emu-lib=/usr/lib/libemu \
-	--with-emu-include=/usr/include \
-	--with-gc-include=/usr/include/gc \
-	--enable-nl \
-	--with-nl-include=/usr/include/libnl3 \
-	--with-nl-lib=/usr/lib \
-	--with-lcfg-lib=/opt/dionaea/lib/ \
-	--with-curl-config=/usr/bin/
-make
-sudo make install
-useradd -r -s /bin/false dionaea
-chown -R dionaea:dionaea /opt/dionaea/
-echo "Making Dionaea honeypot database..."
-mkdir /opt/dionaea/var/dionaea/scripts
-wget https://raw.githubusercontent.com/phage-nz/malware-hunting/master/honeypot/generate_user_db.py -P /opt/dionaea/var/dionaea/scripts
-wget https://raw.githubusercontent.com/phage-nz/malware-hunting/master/honeypot/wordlist.txt -P /opt/dionaea/var/dionaea/scripts
-chown -R dionaea:dionaea /opt/dionaea/var/dionaea/scripts
-chmod +x /opt/dionaea/var/dionaea/scripts/generate_user_db.py
-sudo -u dionaea touch /opt/dionaea/var/dionaea/target_db.sqlite
-sudo -u dionaea /opt/dionaea/var/dionaea/scripts/generate_user_db.py
-echo "Fixing up Dionaea config files..."
-sed -e '/errors.filename/ s/^#*/#/' -i /opt/dionaea/etc/dionaea/dionaea.cfg
-sed -e '/errors.levels/ s/^#*/#/' -i /opt/dionaea/etc/dionaea/dionaea.cfg
-sed -e '/errors.domains/ s/^#*/#/' -i /opt/dionaea/etc/dionaea/dionaea.cfg
-sed -i 's/^\(default.levels=\).*/\1all,-debug/' /opt/dionaea/etc/dionaea/dionaea.cfg
-sed -i 's/# ssl.default/ssl.default/g' /opt/dionaea/etc/dionaea/dionaea.cfg
-sed -i 's/^\(ssl.default.c=\).*/\1'"$SSL_C"'/' /opt/dionaea/etc/dionaea/dionaea.cfg
-sed -i 's/^\(ssl.default.cn=\).*/\1'"$SSL_CN"'/' /opt/dionaea/etc/dionaea/dionaea.cfg
-sed -i 's/^\(ssl.default.o=\).*/\1'"$SSL_O"'/' /opt/dionaea/etc/dionaea/dionaea.cfg
-sed -i 's/^\(ssl.default.ou=\).*/\1'"$SSL_OU"'/' /opt/dionaea/etc/dionaea/dionaea.cfg
-sed -i '9,10 s/^#//' /opt/dionaea/etc/dionaea/services-available/mysql.yaml
-sed -i 's/\/path\/to\/cc_info.sqlite/\/opt\/dionaea\/var\/dionaea\/target_db.sqlite/g' /opt/dionaea/etc/dionaea/services-available/mysql.yaml
-sed -i '4,6 s/^#//' /opt/dionaea/etc/dionaea/services-available/pptp.yaml
-sed -e '/self.root_path\s*=\s*/ s/^#*/#/' -i /opt/dionaea/lib/dionaea/python/dionaea/sip/extras.py
-sed -e '/self.users\s*=\s*/ s/^#*/#/' -i /opt/dionaea/lib/dionaea/python/dionaea/sip/extras.py
-sed -i 's/sqlite3.connect(self.users)/sqlite3.connect("\/opt\/dionaea\/var\/dionaea\/sipaccounts.sqlite")/g' /opt/dionaea/lib/dionaea/python/dionaea/sip/extras.py
-sed -i 's/"OemDomainName", "WORKGROUP"/"OemDomainName", "'"$DOMAIN"'"/g' /opt/dionaea/lib/dionaea/python/dionaea/smb/include/smbfields.py
-sed -i 's/"ServerName", "HOMEUSER-3AF6FE"/"ServerName", "'"$SERVER"'"/g' /opt/dionaea/lib/dionaea/python/dionaea/smb/include/smbfields.py
-sed -i  's/^\(\s*r\.VersionToken\.TokenType\s*=\s*\).*$/\10xAA/' /opt/dionaea/lib/dionaea/python/dionaea/mssql/mssql.py
-echo "Enabling Dionaea p0f handler..."
-sudo -u dionaea ln -s /opt/dionaea/etc/dionaea/ihandlers-available/p0f.yaml /opt/dionaea/etc/dionaea/ihandlers-enabled
-echo "Making a folder for SIP pcap's..."
-sudo -u dionaea mkdir -p /opt/dionaea/var/dionaea/rtp/default
-echo "Installing Cowrie..."
-mkdir /opt/cowrie/
-git clone https://github.com/micheloosterhof/cowrie /opt/cowrie/
-cp /opt/cowrie/cowrie.cfg.dist /opt/cowrie/cowrie.cfg
-echo "Making Cowrie user and applying permissions..."
-useradd -r -s /bin/false cowrie
-mkdir -p /var/run/cowrie
-chown -R cowrie:cowrie /opt/cowrie/
-chown -R cowrie:cowrie /var/run/cowrie/
-echo "Fixing up the Cowrie config file..."
-sed -i 's/^\(hostname\s*=\s*\).*/\1'"$SERVER"'/' /opt/cowrie/cowrie.cfg
-echo "Making the Cowrie filesystem..."
-/opt/cowrie/bin/createfs
-echo "Making logrotate scripts..."
-wget https://raw.githubusercontent.com/phage-nz/malware-hunting/master/honeypot/dionaea.logrotate -O /etc/logrotate.d/dionaea
-wget https://raw.githubusercontent.com/phage-nz/malware-hunting/master/honeypot/cowrie.logrotate -O /etc/logrotate.d/cowrie
-echo "Setting all services to autostart..."
-wget https://raw.githubusercontent.com/phage-nz/malware-hunting/master/honeypot/p0f.init -O /etc/init.d/p0f
-wget https://raw.githubusercontent.com/phage-nz/malware-hunting/master/honeypot/cowrie.init -O /etc/init.d/cowrie
-chmod +x /etc/init.d/p0f
-chmod +x /etc/init.d/cowrie
-update-rc.d dionaea defaults
-update-rc.d p0f defaults
-update-rc.d cowrie defaults
-echo "Redirecting port 22 to Cowrie. You will need to re-establish your SSH session on port 8925 after the service reloads."
-sed -i 's/Port 22/Port 8925/g' /etc/ssh/sshd_config
-service ssh reload
-iptables -t nat -A PREROUTING -p tcp --dport 22 -j REDIRECT --to-port 2222
-iptables-save > /etc/iptables.rules
-echo '#!/bin/sh' >> /etc/network/if-up.d/iptablesload 
-echo 'iptables-restore < /etc/iptables.rules' >> /etc/network/if-up.d/iptablesload 
-echo 'exit 0' >> /etc/network/if-up.d/iptablesload
-chmod +x /etc/network/if-up.d/iptablesload
-echo "Starting services..."
-/etc/init.d/p0f start
-/etc/init.d/dionaea start
-/etc/init.d/cowrie start
-echo "Exiting session..."
+# Install must be done as root.
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root!"
+  exit 1
+fi
+
+# Install dependencies if either Dionaea or Cowrie is to be installed.
+if [ "$INSTALL_DIONAEA" == "yes" ] || [ "$INSTALL_COWRIE" == "yes" ] ; then
+  echo "Updating server..."
+  apt update
+  apt upgrade -y
+  echo "Installing dependencies..."
+  apt install autoconf automake build-essential check cython3 git libcurl4-openssl-dev libemu-dev libev-dev libffi-dev libglib2.0-dev libgmp-dev libloudmouth1-dev libmpc-dev libmpfr-dev libnetfilter-queue-dev libnl-dev libpcap-dev libpython-dev libreadline-dev libsqlite3-dev libssl-dev libtool libudns-dev libxml2-dev libxslt1-dev p0f python-dev python-pip python-software-properties python-virtualenv python3 python3-dev python3-yaml software-properties-common -y
+  # Cowrie requires the latest version of Python OpenSSL.
+  apt remove python-openssl
+  pip install pyopenssl
+else
+  echo "Nothing to be installed."
+  exit 0
+fi
+
+# Install Dionaea.
+if [ "$INSTALL_DIONAEA" == "yes" ]; then
+  echo "Installing Dionaea..."
+  cd /opt
+  git clone -b WannaCry https://github.com/DinoTools/dionaea.git
+  cd dionaea
+  git clone https://github.com/gento/liblcfg
+  cd liblcfg/code
+  autoreconf -vi
+  ./configure --prefix=/opt/dionaea
+  make install
+  cd ../..
+  autoreconf -vi
+  ./configure \
+    --disable-werror \
+    --prefix=/opt/dionaea \
+    --with-python=/usr/bin/python3 \
+    --with-cython-dir=/usr/bin \
+    --with-ev-include=/usr/include \
+    --with-ev-lib=/usr/lib \
+    --with-emu-lib=/usr/lib/libemu \
+    --with-emu-include=/usr/include \
+    --with-gc-include=/usr/include/gc \
+    --enable-nl \
+    --with-nl-include=/usr/include/libnl3 \
+    --with-nl-lib=/usr/lib \
+    --with-lcfg-lib=/opt/dionaea/lib/ \
+    --with-curl-config=/usr/bin/
+  make
+  make install
+  echo "Making underprivileged user..."
+  useradd -r -s /bin/false dionaea
+  chown -R dionaea:dionaea /opt/dionaea/
+  mkdir /home/dionaea
+  chown dionaea:dionaea /home/dionaea
+  echo "Making Dionaea honeypot database..."
+  mkdir /opt/dionaea/var/dionaea/scripts
+  wget https://raw.githubusercontent.com/phage-nz/malware-hunting/master/honeypot/generate_user_db.py -P /opt/dionaea/var/dionaea/scripts
+  wget https://raw.githubusercontent.com/phage-nz/malware-hunting/master/honeypot/wordlist.txt -P /opt/dionaea/var/dionaea/scripts
+  chown -R dionaea:dionaea /opt/dionaea/var/dionaea/scripts
+  chmod +x /opt/dionaea/var/dionaea/scripts/generate_user_db.py
+  sudo -u dionaea touch /opt/dionaea/var/dionaea/target_db.sqlite
+  sudo -u dionaea /opt/dionaea/var/dionaea/scripts/generate_user_db.py
+  echo "Fixing up Dionaea config files..."
+  sed -e '/errors.filename/ s/^#*/#/' -i /opt/dionaea/etc/dionaea/dionaea.cfg
+  sed -e '/errors.levels/ s/^#*/#/' -i /opt/dionaea/etc/dionaea/dionaea.cfg
+  sed -e '/errors.domains/ s/^#*/#/' -i /opt/dionaea/etc/dionaea/dionaea.cfg
+  sed -i 's/^\(default.levels=\).*/\1all,-debug/' /opt/dionaea/etc/dionaea/dionaea.cfg
+  sed -i 's/# ssl.default/ssl.default/g' /opt/dionaea/etc/dionaea/dionaea.cfg
+  sed -i 's/^\(ssl.default.c=\).*/\1'"$SSL_C"'/' /opt/dionaea/etc/dionaea/dionaea.cfg
+  sed -i 's/^\(ssl.default.cn=\).*/\1'"$SSL_CN"'/' /opt/dionaea/etc/dionaea/dionaea.cfg
+  sed -i 's/^\(ssl.default.o=\).*/\1'"$SSL_O"'/' /opt/dionaea/etc/dionaea/dionaea.cfg
+  sed -i 's/^\(ssl.default.ou=\).*/\1'"$SSL_OU"'/' /opt/dionaea/etc/dionaea/dionaea.cfg
+  sed -i '9,10 s/^#//' /opt/dionaea/etc/dionaea/services-available/mysql.yaml
+  sed -i 's/\/path\/to\/cc_info.sqlite/\/opt\/dionaea\/var\/dionaea\/target_db.sqlite/g' /opt/dionaea/etc/dionaea/services-available/mysql.yaml
+  sed -i 's/apikey: "........."/apikey: "'"$VT_KEY"'"/g' /opt/dionaea/etc/dionaea/ihandlers-available/virustotal.yaml
+  sed -i '4,6 s/^#//' /opt/dionaea/etc/dionaea/services-available/pptp.yaml
+  sed -e '/self.make_comment/ s/^#*/#/' -i /opt/dionaea/lib/dionaea/python/dionaea/virustotal.py
+  sed -e '/self.root_path\s*=\s*/ s/^#*/#/' -i /opt/dionaea/lib/dionaea/python/dionaea/sip/extras.py
+  sed -e '/self.users\s*=\s*/ s/^#*/#/' -i /opt/dionaea/lib/dionaea/python/dionaea/sip/extras.py
+  sed -i 's/sqlite3.connect(self.users)/sqlite3.connect("\/opt\/dionaea\/var\/dionaea\/sipaccounts.sqlite")/g' /opt/dionaea/lib/dionaea/python/dionaea/sip/extras.py
+  sed -i 's/"OemDomainName", "WORKGROUP"/"OemDomainName", "'"$DOMAIN"'"/g' /opt/dionaea/lib/dionaea/python/dionaea/smb/include/smbfields.py
+  sed -i 's/"ServerName", "HOMEUSER-3AF6FE"/"ServerName", "'"$SERVER"'"/g' /opt/dionaea/lib/dionaea/python/dionaea/smb/include/smbfields.py
+  sed -i  's/^\(\s*r\.VersionToken\.TokenType\s*=\s*\).*$/\10xAA/' /opt/dionaea/lib/dionaea/python/dionaea/mssql/mssql.py
+  echo "Enabling VirusTotal handler..."
+  sudo -u dionaea ln -s /opt/dionaea/etc/dionaea/ihandlers-available/virustotal.yaml /opt/dionaea/etc/dionaea/ihandlers-enabled 
+  echo "Enabling Dionaea p0f handler..."
+  sudo -u dionaea ln -s /opt/dionaea/etc/dionaea/ihandlers-available/p0f.yaml /opt/dionaea/etc/dionaea/ihandlers-enabled
+  echo "Making a folder for SIP pcap's..."
+  sudo -u dionaea mkdir -p /opt/dionaea/var/dionaea/rtp/default
+  echo "Making logrotate script..."
+  wget https://raw.githubusercontent.com/phage-nz/malware-hunting/master/honeypot/dionaea.logrotate -O /etc/logrotate.d/dionaea
+  echo "Setting all services to autostart..."
+  wget https://raw.githubusercontent.com/phage-nz/malware-hunting/master/honeypot/dionaea.init -O /etc/init.d/dionaea
+  wget https://raw.githubusercontent.com/phage-nz/malware-hunting/master/honeypot/p0f.init -O /etc/init.d/p0f
+  chmod +x /etc/init.d/dionaea
+  chmod +x /etc/init.d/p0f
+  update-rc.d dionaea defaults
+  update-rc.d p0f defaults
+  echo "Starting services..."
+  /etc/init.d/p0f start
+  /etc/init.d/dionaea start
+  echo "Dionaea install complete! You may wish to alter the enabled handlers and services in /opt/dionaea/etc/dionaea."
+fi
+
+# Install Cowrie.
+if [ "$INSTALL_COWRIE" == "yes" ]; then
+  echo "Installing Cowrie..."
+  cd /opt
+  git clone https://github.com/micheloosterhof/cowrie
+  cp /opt/cowrie/cowrie.cfg.dist /opt/cowrie/cowrie.cfg
+  echo "Installing Python dependencies..."
+  pip install -r requirements.txt
+  echo "Making underprivileged user..."
+  useradd -r -s /bin/false cowrie
+  mkdir -p /var/run/cowrie
+  chown -R cowrie:cowrie /opt/cowrie/
+  chown -R cowrie:cowrie /var/run/cowrie/
+  mkdir /home/cowrie
+  chown cowrie:cowrie /home/cowrie
+  echo "Fixing up the Cowrie config file..."
+  sed -i 's/^\(hostname\s*=\s*\).*/\1'"$SERVER"'/' /opt/cowrie/cowrie.cfg
+  echo "Making the Cowrie filesystem..."
+  /opt/cowrie/bin/createfs
+  echo "Making logrotate script..."
+  wget https://raw.githubusercontent.com/phage-nz/malware-hunting/master/honeypot/cowrie.logrotate -O /etc/logrotate.d/cowrie
+  echo "Setting service to autostart..."
+  wget https://raw.githubusercontent.com/phage-nz/malware-hunting/master/honeypot/cowrie.init -O /etc/init.d/cowrie
+  chmod +x /etc/init.d/cowrie
+  update-rc.d cowrie defaults
+  echo "Redirecting port 22 to Cowrie. You will need to re-establish your SSH session on port 8925 after the service reloads."
+  sed -i 's/Port 22/Port 8925/g' /etc/ssh/sshd_config
+  service ssh reload
+  iptables -t nat -A PREROUTING -p tcp --dport 22 -j REDIRECT --to-port 2222
+  iptables-save > /etc/iptables.rules
+  echo '#!/bin/sh' >> /etc/network/if-up.d/iptablesload 
+  echo 'iptables-restore < /etc/iptables.rules' >> /etc/network/if-up.d/iptablesload 
+  echo 'exit 0' >> /etc/network/if-up.d/iptablesload
+  chmod +x /etc/network/if-up.d/iptablesload
+  echo "Starting service..."
+  /etc/init.d/cowrie start
+  echo "Cowrie install complete!"
+fi
+
 exit 0
--- honeypot/readme.txt
+++ honeypot/readme.txt
@@ -1,207 +1,116 @@
+Honeypot AutoInstall Script v0.3
+by Chris Campbell
+
+Twitter: @phage_nz
+GitHub: phage-nz
+Blog: https://phage.nz
+
+Installs:
+Dionaea
+p0f
+Cowrie
+
+Tested on Ubuntu 14.04 (EC2 t2.micro instance)
+
+
 Default Install Notes
 """""""""""""""""""""
 
-# Fetch the autoinstall script.
+# Fetch the latest release of the autoinstall script.
 wget https://raw.githubusercontent.com/phage-nz/malware-hunting/master/honeypots/autoinstall.sh
 chmod +x autoinstall.sh
-# Edit variables where required.
+# Edit the variables at the top of the script.
 nano autoinstall.sh
 # Run the script and happy hunting!
-sudo autoinstall.sh
+sudo ./autoinstall.sh
 
-Refer to the manual install notes below for information on where customisations can be made.
+Refer to the notes below for information on where customisations can be made.
 
 Files required for automated Vagrant deployment are also available in ../honeypot-vagrant.
 
 
-Manual Install Notes
-""""""""""""""""""""
+Post-Install Notes
+""""""""""""""""""
+Dionaea
+'''''''
+Essential:
 
-- Spin up t2.micro Ubuntu 14.04 EC2 instance. Create and apply security group with the following ports:
-ftp (21/tcp) 
-http/https (80/tcp and 443/tcp) 
-nameserver (42/tcp) 
-msrpc (135/tcp ) 
-smb (445/tcp) 
-tftp (69/udp) 
-ms-sql (1433/tcp)
-pptp (1723/tcp)
-scada (1883/tcp)
-upnp (1900/udp)
-mysql (port 3306/tcp) 
-sip/sip-tls (5060/tcp+udp and 5061/tcp)
-memcache (11211/tcp)
+- Create and apply security group with the following ports:
+Management SSH (22/TCP) - restricted to your public IP. Only used if Cowrie is not installed.
+FTP (21/TCP) 
+HTTP/HTTPS (80/TCP and 443/TCP) 
+NameServer (42/TCP) 
+MSRPC (135/TCP ) 
+SMB (445/TCP) 
+TFTP (69/UDP) 
+MSSQL (1433/TCP)
+PPTP (1723/TCP)
+SCADA (1883/TCP)
+UPnP (1900/UDP)
+MySQL (port 3306/TCP) 
+SIP/SIP-TLS (5060/TCP+UDP and 5061/TCP)
+Memcache (11211/TCP)
 
-- Relocate the private key:
-mv PrivateKey.pem ~/.ssh
-chmod 400 ~/.ssh/PrivateKey.pem
+- Ensure Dionaea is listening:
+netstat -putan|grep dionaea
 
-- Connect to the instance:
-ssh -i ~/.ssh/PrivateKey.pem ubuntu@ec2-xx-xxx-xxx-xx.us-west-2.compute.amazonaws.com
+- Check the error log in /opt/dionaea/var/dionaea/dionaea-errors.log
 
-- Install Dionaea and p0f:
-sudo apt-get update
-apt-get install software-properties-common python-software-properties p0f autoconf automake check cython3 libcurl4-openssl-dev libemu-dev libev-dev libglib2.0-dev libloudmouth1-dev libnetfilter-queue-dev libnl-dev libpcap-dev libreadline-dev libsqlite3-dev libssl-dev libtool libudns-dev libxml2-dev libxslt1-dev python3 python3-dev python3-yaml -y
-sudo apt-get upgrade -y
-(reboot if required for kernel updates)
-cd /opt
-git clone https://github.com/DinoTools/dionaea
-cd dionaea
-git clone https://github.com/gento/liblcfg
-cd liblcfg/code
-autoreconf -vi
-./configure --prefix=/opt/dionaea
-make install
-cd ..
-cd ..
-autoreconf -vi
-./configure \
-	--disable-werror \
-	--prefix=/opt/dionaea \
-	--with-python=/usr/bin/python3 \
-	--with-cython-dir=/usr/bin \
-	--with-ev-include=/usr/include \
-	--with-ev-lib=/usr/lib \
-	--with-emu-lib=/usr/lib/libemu \
-	--with-emu-include=/usr/include \
-	--with-gc-include=/usr/include/gc \
-	--enable-nl \
-	--with-nl-include=/usr/include/libnl3 \
-	--with-nl-lib=/usr/lib \
-	--with-lcfg-lib=/opt/dionaea/lib/ \
-	--with-curl-config=/usr/bin/
-make
-sudo make install
-useradd -r -s /bin/false dionaea
-chown -R dionaea:dionaea /opt/dionaea/
-mkdir /home/dionaea
-chown dionaea:dionaea /home/dionaea
+- General logs is /opt/dionaea/var/dionaea/dionaea.log and download binaries are in /opt/dionaea/var/dionaea/binaries
 
-- Install sqlite3 command line utility (useful for ad-hoc queries):
-apt-get install sqlite3 -y
 
-- Edit the main config:
-nano /opt/dionaea/etc/dionaea/dionaea.cfg
--- To mitigate issues with the error log not being written to, comment out:
-#errors.filename=/opt/dionaea/var/dionaea/dionaea-errors.log
-#errors.levels=warning,error
-#errors.domains=*
--- Edit as follows (SSL details as example):
-FROM> default.levels=all
-TO> default.levels=all,-debug
+Optional:
 
-TO> ssl.default.c=XX
-TO> ssl.default.cn=XXXX.domain.com
-TO> ssl.default.o=XXXX Inc
-TO> ssl.default.ou=XX
+- The main operational config is /opt/dionaea/etc/dionaea/dionaea.cfg
 
-- Create the MySQL target database:
--- Copy out wordlist.txt and generate_user_db.py to your home folder.
-chmod +x generate_user_db.py
-sudo -u dionaea touch /opt/dionaea/var/dionaea/target_db.sqlite
-sudo -u dionaea ./generate_user_db.py
+- To generate a fresh MySQL target database:
 
-- Edit the MySQL config:
-nano /opt/dionaea/etc/dionaea/services-available/mysql.yaml
--- Uncomment the following two lines and correct the path of the user database:
-psn:
-  path: "/opt/dionaea/var/dionaea/target_db.sqlite"
+python /opt/dionaea/var/dionaea/scripts/generate_user_db.py
 
-- Edit the PPTP config:
+- To emulate different PPTP devices:
+
 nano /opt/dionaea/etc/dionaea/services-available/pptp.yaml
+
 -- Uncomment three lines under the device you wish to emulate, e.g.
 firmware_revision: 4608
 hostname: PIX
 vendor_name: Cisco Systems
 
-- Edit the SIP script:
-nano /opt/dionaea/lib/dionaea/python/dionaea/sip/extras.py
--- Comment out:
-#self.root_path = os.getcwd()
-#self.users = os.path.join(self.root_path, config.get("users", "var/dionaea/sipaccounts.sqlite"))
--- Edit both occurences of:
-FROM> sqlite3.connect(self.users)
-TO> sqlite3.connect("/opt/dionaea/var/dionaea/sipaccounts.sqlite")
-
-- Edit the SMB script:
-nano /opt/dionaea/lib/dionaea/python/dionaea/smb/include/smbfields.py
--- Edit as follows (where X is specified, define your own entry):
-FROM>        ConditionalField(UnicodeNullField(
-FROM>            "OemDomainName", "WORKGROUP"), lambda x: not x.Capabilities & CAP_EXTENDED_SECURITY),
-
-TO>        ConditionalField(UnicodeNullField(
-TO>            "OemDomainName", "XXXX"), lambda x: not x.Capabilities & CAP_EXTENDED_SECURITY),
-
-FROM>        ConditionalField(UnicodeNullField(
-FROM>            "ServerName", "HOMEUSER-XXXX"), lambda x: not x.Capabilities & CAP_EXTENDED_SECURITY),
-
-TO>        ConditionalField(UnicodeNullField(
-TO>            "ServerName", "XXXX"), lambda x: not x.Capabilities & CAP_EXTENDED_SECURITY),
-
-
 - It's not necessary to edit the FTP script in later releases. You can edit the welcome banner in /opt/dionaea/etc/dionaea/services-available/ftp.yaml
 
-- Edit the MSSQL script:
+- To change the MSSQL version ID, edit the MSSQL script:
+
 nano /opt/dionaea/lib/dionaea/python/dionaea/mssql/mssql.py
+
 -- Edit as follows:
 FROM> r.VersionToken.TokenType = 0x00
 TO> r.VersionToken.TokenType = 0xAA
 
 ?> Refer to http://www.freetds.org/tds.html#responses for options.
 
-- Insert your VirusTotal API key into: /opt/dionaea/etc/dionaea/ihandlers-available/virustotal.yaml
-- Comment out the following line in /opt/dionaea/lib/dionaea/python/dionaea/virustotal.py if you wish to disable automatic commenting on uploaded files:
+- Comment out the following line in /opt/dionaea/lib/dionaea/python/dionaea/virustotal.py if you wish to disable automatic commenting of files uploaded to VirusTotal (done by default):
 
 self.make_comment(sf[0], sf[1], sf[2], 'comment')
 
-- Enable the VirusTotal plugin:
 
-sudo -u dionaea ln -s /opt/dionaea/etc/dionaea/ihandlers-enabled/virustotal.yaml /opt/dionaea/etc/dionaea/ihandlers-enabled
+Cowrie
+''''''
+Essential:
 
-- Make a directory for SIP pcap's:
-sudo -u dionaea mkdir -p /opt/dionaea/var/dionaea/rtp/default
+- Create and apply security group with the following ports:
+Cowrie SSH (22/TCP)
+Management SSH (8925/TCP) - restrict to your public IP.
 
-- Create a logrotate script to rotate the logs on a daily basis using dionaea.logrotate (copy to /etc/logrotate.d/dionaea)
+- Ensure Cowrie is listening:
+netstat -putan|grep 2222
 
-- Enable Dionaea p0f handler:
-sudo -u dionaea ln -s /opt/dionaea/etc/dionaea/ihandlers-available/p0f.yaml /opt/dionaea/etc/dionaea/ihandlers-enabled
+- Test:
+ssh root@<server name>
 
-- Make p0f init script using p0f.init (copy to /etc/init.d/p0f) then:
-chmod +x /etc/init.d/p0f
+-- Use a password defined in userdb.txt
 
-- Set all to autostart:
-update-rc.d dionaea defaults
-update-rc.d p0f defaults
 
-- Start the honeypot:
-/etc/init.d/p0f start
-/etc/init.d/dionaea start
-
-- Make sure that Dionaea is listening on all expected ports:
-netstat -putan|grep dionaea
-
-- Check the error log in /opt/dionaea/var/dionaea/dionaea-errors.log
-
-- Add Custom TCP rule to AWS SSH security group for port 8925.
-
-- Edit listen port of SSH service:
-nano /etc/ssh/sshd_config
-
-FROM> Port 22
-TO> Port 8925
-
-- Reload SSH service:
-service ssh reload
-
-- Establish new SSH session.
-
-- Install dependencies:
-apt-get install python-twisted python-configparser python-crypto python-pyasn1 python-gmpy2 python-mysqldb python-zope.interface
-
-- Install Cowrie:
-mkdir /opt/cowrie/
-git clone https://github.com/micheloosterhof/cowrie /opt/cowrie/
-cp /opt/cowrie/cowrie.cfg.dist /opt/cowrie/cowrie.cfg
+Optional:
 
 - Edit the host name:
 nano /opt/cowrie/cowrie.cfg
@@ -209,7 +118,7 @@ nano /opt/cowrie/cowrie.cfg
 FROM>srv03
 TO>*whatever you want*
 
-- Create filesystem:
+- Recreate filesystem:
 /opt/cowrie/bin/createfs
 
 - Add some more root passwords if desired:
@@ -222,42 +131,3 @@ e.g. root:0:toor
 -- Format for a denied password:
 <user>:0:!<password>
 e.g. root:0:!toor
-
-- Create cowrie user:
-useradd -r -s /bin/false cowrie
-
-- Apply directory permissions:
-mkdir -p /var/run/cowrie
-chown -R cowrie:cowrie /opt/cowrie/
-chown -R cowrie:cowrie /var/run/cowrie/
-
-- Redirect port 22 to 2222:
-iptables -t nat -A PREROUTING -p tcp --dport 22 -j REDIRECT --to-port 2222
-
-- Make changes persistent:
-iptables-save > /etc/iptables.rules
-echo '#!/bin/sh' >> /etc/network/if-up.d/iptablesload 
-echo 'iptables-restore < /etc/iptables.rules' >> /etc/network/if-up.d/iptablesload 
-echo 'exit 0' >> /etc/network/if-up.d/iptablesload
-chmod +x /etc/network/if-up.d/iptablesload
-
-- Copy init.d script in place as /etc/init.d/cowrie
-
-- Allow init script to execute:
-chmod +x /etc/init.d/cowrie
-
-- Set to autostart:
-update-rc.d cowrie defaults
-
--Copy logrotate script to /etc/logrotate.d/cowrie
-
-- Start Cowrie:
-/etc/init.d/cowrie start
-
-- Ensure it's listening:
-netstat -putan|grep 2222
-
-- Test:
-ssh root@<server name>
-
-- Use a password defined in userdb.txt
