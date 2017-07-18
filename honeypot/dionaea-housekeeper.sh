39c50d0eb976a3314ef373b1374009892c2f7c7e
Chris <phage-nz@users.noreply.github.com>
2017-07-19 09:16:20 +1200

    Prune old archives.

--- honeypot/dionaea-housekeeper.sh
+++ honeypot/dionaea-housekeeper.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+find /opt/dionaea/var/dionaea/bistreams/* -type f -name '*.tar.gz' -mtime +7 -exec rm {} \;
+
 date=$(date -d "yesterday" '+%Y-%m-%d')
 bistreams=/opt/dionaea/var/dionaea/bistreams/$date
 if [ -d "$bistreams" ]; then
