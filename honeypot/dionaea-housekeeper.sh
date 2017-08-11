9a21f9eb2bdd5eb372957249c25844bf952e7a33
Chris <phage-nz@users.noreply.github.com>
2017-08-12 09:17:38 +1200

    Housekeeper update.

--- honeypot/dionaea-housekeeper.sh
+++ honeypot/dionaea-housekeeper.sh
@@ -4,5 +4,6 @@ find /opt/dionaea/var/dionaea/bistreams/* -type f -name '*.tar.gz' -mtime +7 -ex
 date=$(date -d "yesterday" '+%Y-%m-%d')
 bistreams=/opt/dionaea/var/dionaea/bistreams/$date
 if [ -d "$bistreams" ]; then
-  tar -czf - -C $bistreams . > /opt/dionaea/var/dionaea/bistreams/$date.tar.gz && rm -rf $bistreams
+  tar -czf - -C $bistreams . > /opt/dionaea/var/dionaea/bistreams/$date.tar.gz
+  rm -rf $bistreams
 fi
