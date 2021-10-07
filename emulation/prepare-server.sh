4d394a7b30aa471a76c11e4396d0ee0d0028586f
Chris <phage-nz@users.noreply.github.com>
2021-10-07 17:12:25 +1300

    Update prepare-server.sh

--- emulation/prepare-server.sh
+++ emulation/prepare-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "---------------------------------------------------"
-echo "[*] EMULATION SERVER PREPARATION SCRIPT - 19/7/20"
+echo "[*] EMULATION SERVER PREPARATION SCRIPT - 07/10/21"
 echo '[*] "Train like you fight..."'
 echo '[*] https://github.com/phage-nz/infosec-bazaar/tree/master/emulation'
 echo "---------------------------------------------------"
@@ -27,12 +27,13 @@ if [[ $UPGRADE_OS = "TRUE" ]]; then
 fi
 echo "---------------------------------------------------"
 echo "[*] Installing OS pre-requisites..."
-add-apt-repository -y ppa:certbot/certbot
-add-apt-repository -y ppa:ubuntu-toolchain-r/test
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-stretch-prod stretch main" > /etc/apt/sources.list.d/dotnetdev.list
+add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O /tmp/packages-microsoft-prod.deb
+dpkg -i /tmp/packages-microsoft-prod.deb
 apt-get update
-apt install -y apache2 autoconf build-essential certbot default-jdk dotnet-sdk-3.1 g++ git libffi-dev libssl-dev libssl1.1 libxml2-dev make mingw-w64 mingw-w64-common nmap p7zip-full python-dev python-pip python-setuptools python-virtualenv python3-certbot-apache python3-dev python3-pip python3-setuptools python3.7-dev ruby ruby-dev software-properties-common swig unzip zlib1g-dev
+apt install -y apache2 autoconf build-essential certbot default-jdk docker-ce dotnet-sdk-3.1 g++ git libffi-dev libssl-dev libssl1.1 libxml2-dev make mingw-w64 mingw-w64-common net-tools nmap p7zip-full python3-certbot-apache python3-dev python3-pip python3-setuptools python3-testresources ruby ruby-dev software-properties-common swig unzip zlib1g-dev
+curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 gem install bundle
 if [[ $INSTALL_RDP = "TRUE" ]]; then
     echo "[-] Including remote desktop packages..."
@@ -60,9 +61,9 @@ cd /opt/Covenant/Covenant
 dotnet build
 echo "---------------------------------------------------"
 echo "[*] Installing Empire..."
+echo "[!] Note: the install script will require your input at several points."
 git clone https://github.com/BC-SECURITY/Empire /opt/Empire
 cd /opt/Empire
-pip3 install -r setup/requirements.txt
 export STAGING_KEY=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 ./setup/install.sh
 mkdir beacon2empire && cd beacon2empire
@@ -73,8 +74,10 @@ echo "---------------------------------------------------"
 echo "[*] Setting up Exploit DB"
 git clone https://github.com/offensive-security/exploit-database /opt/exploit-db
 echo "---------------------------------------------------"
-echo "[*] Setting up Invoke-Obfuscation"
-git clone https://github.com/danielbohannon/Invoke-Obfuscation /opt/Invoke-Obfuscation
+echo "[*] Setting up Obfuscators"
+mkdir /opt/Obfuscators
+git clone https://github.com/danielbohannon/Invoke-Obfuscation /opt/Obfuscators/Invoke-Obfuscation
+git clone https://github.com/CBHue/PyFuscation /opt/Obfuscators/PyFuscation
 echo "---------------------------------------------------"
 echo "[*] Installing Koadic"
 git clone https://github.com/zerosum0x0/koadic /opt/Koadic
@@ -83,8 +86,10 @@ pip3 install -r requirements.txt
 echo "---------------------------------------------------"
 echo "[*] Installing Merlin"
 mkdir /opt/Merlin && cd /opt/Merlin
-wget https://github.com/Ne0nd0g/merlin/releases/download/v0.8.0/merlinServer-Linux-x64-v0.8.0.BETA.7z
-7z x -pmerlin merlinServer-Linux-x64-v0.8.0.BETA.7z && rm merlinServer-Linux-x64-v0.8.0.BETA.7z
+wget https://github.com/Ne0nd0g/merlin/releases/download/v1.1.0/merlinServer-Linux-x64.7z
+wget https://github.com/Ne0nd0g/merlin/releases/download/v1.1.0/merlinAgent-Linux-x64.7z
+wget https://github.com/Ne0nd0g/merlin/releases/download/v1.1.0/merlinAgent-Windows-x64.7z
+7z x -pmerlin merlinServer-Linux-x64.7z && rm merlinServer-Linux-x64.7z
 mv merlinServer-Linux-x64 merlinServer
 echo "---------------------------------------------------"
 echo "[*] Installing Metasploit..."
@@ -122,24 +127,33 @@ cat > modlishka.config << EOF
 }
 EOF
 echo "---------------------------------------------------"
-echo "[*] Installing PoshC2..."
-cd /tmp
-wget https://raw.githubusercontent.com/nettitude/PoshC2/master/Install.sh -O poshinstall
-chmod +x poshinstall && ./poshinstall && rm poshinstall
+echo "[*] Installing Mythic"
+git clone https://github.com/its-a-feature/Mythic /opt/Mythic
+./mythic-cli install github https://github.com/MythicAgents/Apollo
+./mythic-cli install github https://github.com/MythicAgents/Medusa
+./mythic-cli install github https://github.com/MythicC2Profiles/dns
+./mythic-cli install github https://github.com/MythicC2Profiles/dynamichttp
+./mythic-cli install github https://github.com/MythicC2Profiles/http
+./mythic-cli install github https://github.com/MythicC2Profiles/websocket
 echo "---------------------------------------------------"
 echo "[*] Installing SILENTTRINITY"
 git clone https://github.com/byt3bl33d3r/SILENTTRINITY /opt/SILENTTRINITY
 cd /opt/SILENTTRINITY
-python3.7 -m pip install -r requirements.txt
+pip3 install -r requirements.txt
 echo "---------------------------------------------------"
 echo "[*] Installing Sliver..."
 mkdir /opt/Sliver && cd /opt/Sliver
-wget https://github.com/BishopFox/sliver/releases/download/v1.0.3-beta/sliver-server_linux.zip
+wget https://github.com/BishopFox/sliver/releases/download/v1.4.21/sliver-client_linux.zip
 unzip sliver-server_linux.zip && rm sliver-server_linux.zip
 echo "---------------------------------------------------"
 echo "[*] Installing SpiderFoot"
 git clone https://github.com/smicallef/spiderfoot /opt/SpiderFoot
 cd /opt/SpiderFoot
+pip3 install -r requirements.txt
+echo "---------------------------------------------------"
+echo "[*] Installing TrevorC2"
+git clone https://github.com/trustedsec/trevorc2/ /opt/TrevorC2
+cd /opt/TrevorC2
 pip3 install -r requirements.txt
 echo "---------------------------------------------------"
 echo "[*] Fetching credential access tools..."
@@ -159,13 +173,13 @@ wget https://github.com/AlessandroZ/BeRoot/archive/master.zip -O privesc/BeRoot.
 wget https://github.com/l0ss/Grouper2/releases/download/0.9.62/Grouper2.exe -O privesc/Grouper2.exe
 wget https://raw.githubusercontent.com/411Hall/JAWS/master/jaws-enum.ps1 -O privesc/jaws.ps1
 wget https://github.com/AlessandroZ/LaZagne/releases/download/2.4.3/lazagne.exe -O privesc/lazagne.exe
-wget https://github.com/gentilkiwi/mimikatz/releases/download/2.2.0-20200519/mimikatz_trunk.zip -O privesc/mimikatz.zip
+wget https://github.com/gentilkiwi/mimikatz/releases/download/2.2.0-20210810-2/mimikatz_trunk.zip -O privesc/mimikatz.zip
 wget https://raw.githubusercontent.com/M4ximuss/Powerless/master/Powerless.bat -O privesc/Powerless.bat
 wget https://raw.githubusercontent.com/BloodHoundAD/BloodHound/master/Ingestors/SharpHound.ps1 -O privesc/SharpHound.ps1
 wget https://raw.githubusercontent.com/carlospolop/privilege-escalation-awesome-scripts-suite/master/winPEAS/winPEASbat/winPEAS.bat -O privesc/winPEAS.bat
-wget https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite/raw/master/winPEAS/winPEASexe/winPEAS/bin/Obfuscated%20Releases/winPEASx64.exe -O privesc/winPEASx64.exe
-wget https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite/raw/master/winPEAS/winPEASexe/winPEAS/bin/Obfuscated%20Releases/winPEASx86.exe -O privesc/winPEASx86.exe
-wget https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite/raw/master/winPEAS/winPEASexe/winPEAS/bin/Obfuscated%20Releases/winPEASany.exe -O privesc/winPEASany.exe
+wget https://github.com/carlospolop/PEASS-ng/raw/master/winPEAS/winPEASexe/binaries/Obfuscated%20Releases/winPEASx64.exe -O privesc/winPEASx64.exe
+wget https://github.com/carlospolop/PEASS-ng/raw/master/winPEAS/winPEASexe/binaries/Obfuscated%20Releases/winPEASx86.exe -O privesc/winPEASx86.exe
+wget https://github.com/carlospolop/PEASS-ng/raw/master/winPEAS/winPEASexe/binaries/Obfuscated%20Releases/winPEASany.exe -O privesc/winPEASany.exe
 echo "---------------------------------------------------"
 echo "[*] Fetching reconnaissance tools..."
 mkdir recon
@@ -178,6 +192,7 @@ wget https://raw.githubusercontent.com/tennc/webshell/master/net-friend/aspx/asp
 wget https://raw.githubusercontent.com/Cn33liz/MSBuildShell/master/MSBuildShell.csproj -O MSBuildShell.csproj
 wget http://nmap.org/dist/ncat-portable-5.59BETA1.zip -O ncat.zip
 wget https://raw.githubusercontent.com/mIcHyAmRaNe/wso-webshell/master/wso.php -O webshell/wso.txt
+wget https://raw.githubusercontent.com/WhiteWinterWolf/wwwolf-php-webshell/master/webshell.php -O webshell/wolf.txt
 echo "---------------------------------------------------"
 echo "[*] Fetching other utilities..."
 mkdir util
