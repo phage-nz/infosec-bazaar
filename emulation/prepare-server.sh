a102ae233b2c7eb37e9b012814cec1dfac2d2cb6
Chris <phage-nz@users.noreply.github.com>
2023-04-26 14:47:44 +1200

    Havoc install fix.

--- emulation/prepare-server.sh
+++ emulation/prepare-server.sh
@@ -107,13 +107,15 @@ echo "---------------------------------------------------"
 echo "[*] Installing Havoc"
 apt install -y libfontconfig1 libglu1-mesa-dev libgtest-dev libspdlog-dev libboost-all-dev libncurses5-dev libgdbm-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev libbz2-dev mesa-common-dev qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools libqt5websockets5 libqt5websockets5-dev qtdeclarative5-dev qtbase5-dev libqt5websockets5-dev libspdlog-dev libboost-all-dev
 git clone https://github.com/HavocFramework/Havoc.git /opt/Havoc
-cd /opt/Havoc/Client
-make
 cd /opt/Havoc/Teamserver
 go mod download golang.org/x/sys
 go mod download github.com/ugorji/go
 wget https://musl.cc/x86_64-w64-mingw32-cross.tgz -O /tmp/mingw-musl.tgz
+if [ ! -d "data" ]; then
+    mkdir data
+fi
 tar zxvf /tmp/mingw-musl.tgz -C data
+cd /opt/Havoc
 make
 echo "---------------------------------------------------"
 echo "[*] Setting up Merlin"
